05ec9cff48fa0d508c6b0a0e040d3106339fbfce
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-08-04 16:29:07 -0300

    Atividade 01 - Criar um programa capaz de converter temperaturas entre diferentes unidades.

---
+++ Aula-03/Exercicios/Atividade-01/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double Celsius = 0;
+        double Fahrenheit = 0;
+
+        Console.WriteLine("===========================================");
+        Console.Write("Digite a temperatura em Celsius: ");
+
+        Celsius = double.Parse(Console.ReadLine());        
+        Fahrenheit = (Celsius * 9 / 5) + 32;
+
+        Console.WriteLine("===========================================");
+        Console.WriteLine($"{Celsius}°C equivalem a {Fahrenheit}°F");
+        Console.WriteLine("===========================================");
+    }
+}
