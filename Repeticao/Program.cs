d3b0ca7044fdd7fc65479f6a57984ed94569249c
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-08-04 13:53:00 -0300

    estruturtas de reperição e condicionais

---
+++ Repeticao/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+            Sintaxe:
+
+                Enquanto uma condição for verdadeira
+                    Execute um conjunto de comandos
+
+            for -> Quando sabemos a quantidade de repetições.
+            while -> Quando a repetição depende de uma condição.
+            do ... while -> Quando precisa executar aoi menos uma 
+                            vez o bloco de código.
+        */
+
+        // Laço For: Imprimir nuimeors de 1 até o 10
+
+        int numeros = 1;
+
+        Console.WriteLine("Imprimir numeros de 1 até 10, utilizandoo laço for.");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
--- Repeticao/obj/Debug/net10.0/Repeticao.AssemblyInfo.cs
+++ Repeticao/obj/Debug/net10.0/Repeticao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repeticao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3c63559a26d3fd649f118883d786364e21c8d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c03348799007aec631fb7b9d6672f5ce7d336e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repeticao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repeticao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
