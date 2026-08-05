de358bc097df5218b837a7ca8980f0f2f9064559
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-08-05 08:11:25 -0300

    Atividade 03 - Criar um sistema que calcule a média de um aluno e informe sua situação.

--- Aula-03/Exercicios/Atividade-03/Program.cs
+++ Aula-03/Exercicios/Atividade-03/Program.cs
@@ -5,16 +5,45 @@
         string nome;
         double nota1;
         double nota2;
+        double nota3;
+        double math;
 
+        Console.WriteLine("===========================================");
         Console.Write("Nome do Aluno: ");
         nome = Console.ReadLine();
-        Console.Write("Nota1");
+
+        Console.Write("Nota1: ");
         nota1 = double.Parse(Console.ReadLine());
-        Console.Write("Nota2");
+
+        Console.Write("Nota2: ");
         nota2 = double.Parse(Console.ReadLine());
 
+        Console.Write("Nota3: ");
+        nota3 = double.Parse(Console.ReadLine());
 
+        math = (nota1 + nota2 + nota3) / 3;
 
-
+        if (math >= 7)
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine($"Estudante: {nome}");
+            Console.WriteLine($"Média alcançada: {math:f1}");
+            Console.WriteLine($"Situação Final: APROVADO(A)!");
+            Console.WriteLine("===========================================");
+        } else if (math >= 5)
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine($"Estudante: {nome}");
+            Console.WriteLine($"Média alcançada: {math:f1}");
+            Console.WriteLine($"Situação Final: RECUPERAÇÃO!");
+            Console.WriteLine("===========================================");
+        } else
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine($"Estudante: {nome}");
+            Console.WriteLine($"Média alcançada: {math:f1}");
+            Console.WriteLine($"Situação Final: REPROVADO(A)!!");
+            Console.WriteLine("===========================================");
+        }
     }
 }
--- Aula-03/Exercicios/Atividade-03/obj/Debug/net10.0/Atividade-03.AssemblyInfo.cs
+++ Aula-03/Exercicios/Atividade-03/obj/Debug/net10.0/Atividade-03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade-03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b57a2dcb0469fef5129d93467bc99a8837905ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f075e08b16a2ce13895b88d300eef571b4af1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade-03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade-03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
