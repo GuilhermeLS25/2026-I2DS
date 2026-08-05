0c8847137b3035b8eb7800b123f70d5d1d4ce8c0
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-08-05 15:31:22 -0300

    Introdução ao Principios de POO

--- Aula-03/Exercicios/Atividade-07/obj/Debug/net10.0/Atividade-07.AssemblyInfo.cs
+++ Aula-03/Exercicios/Atividade-07/obj/Debug/net10.0/Atividade-07.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade-07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f075e08b16a2ce13895b88d300eef571b4af1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a2b7b3247ba8d78e55f4f35901ad444e362233")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade-07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade-07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
