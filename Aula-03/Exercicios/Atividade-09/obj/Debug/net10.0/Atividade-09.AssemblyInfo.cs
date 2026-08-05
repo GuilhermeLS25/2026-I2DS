55ae31512ad727551003fc76e261e0637adef45e
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-08-05 16:02:23 -0300

    Introdução aos pilares do POO

--- Aula-03/Exercicios/Atividade-09/obj/Debug/net10.0/Atividade-09.AssemblyInfo.cs
+++ Aula-03/Exercicios/Atividade-09/obj/Debug/net10.0/Atividade-09.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade-09")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a2b7b3247ba8d78e55f4f35901ad444e362233")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0949af79c501db8ae3ea4b9022d37692224de19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade-09")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade-09")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
