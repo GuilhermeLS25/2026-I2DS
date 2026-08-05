1797e760b6201fec254da6cf6dfa467526d51996
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-08-05 10:35:26 -0300

    Atividade 07 - Criar um sistema para analisar notas de uma turma.

--- Aula-03/Exercicios/Atividade-07/Program.cs
+++ Aula-03/Exercicios/Atividade-07/Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        double[] notas = new double[3];
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            Console.Write($"Digite a nota do aluno {i + 1}: ");
+            notas[i] = double.Parse(Console.ReadLine());
+        }
+
+        double maior = notas[0];
+        double menor = notas[0];
+        double soma = 0;
+        int aprovados = 0;
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            soma += notas[i];
+
+            if (notas[i] > maior)
+            {
+                maior = notas[i];
+            }
+            if (notas[i] < menor)
+            {
+                menor = notas[i];
+            }
+            if (notas[i] >= 7)
+            {
+                aprovados++;
+            }
+        }
+
+        double media = soma / notas.Length;
+
+        Console.WriteLine("===== Resultado da Turma =====");
+        Console.WriteLine($"Maior nota: {maior}");
+        Console.WriteLine($"Menor nota: {menor}");
+        Console.WriteLine($"Média total da turma: {media}");
+        Console.WriteLine($"Quantidade de alunos Aprovados: {aprovados}");
+    }
+}
