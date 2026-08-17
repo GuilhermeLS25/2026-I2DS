ebb3e3ba57bd0429fd96df43cf08a9618a62a5d6
GuilhermeLS25 <ds.guilhermelima@gmail.com>
2026-08-17 08:19:17 -0300

    Refactor project paths and update assembly information for Aula-04 and Aula-05
    - Updated project file paths in the NuGet cache and related files to reflect new directory structure.
    - Changed assembly informational version for Polimorfismo and Produto projects.
    - Adjusted paths in project.assets.json and other configuration files to match Windows environment.
    - Updated binary files for Produto project after recompilation.
    - Ensured all paths are consistent with the new user directory structure.

--- Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
+++ Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c19230c2555f75e1cee00d66269b3175dd1c2ff0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25b76474818ed28c5b7d595b0fb8efdf27dc57a")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aula-04/04-Polimorfismo/Polimorfismo/obj/Debug/net10.0/Polimorfismo.AssemblyInfo.cs
+++ Aula-04/04-Polimorfismo/Polimorfismo/obj/Debug/net10.0/Polimorfismo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Polimorfismo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c19230c2555f75e1cee00d66269b3175dd1c2ff0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25b76474818ed28c5b7d595b0fb8efdf27dc57a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Polimorfismo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Polimorfismo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
